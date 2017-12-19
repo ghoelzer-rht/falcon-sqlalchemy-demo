eada44331cfd51fa75ec150309a604df4e0e3c64
Greg Hoelzer <ghoelzer@redhat.com>
2017-12-19 16:21:45 -0600

    Update to run on Port 8080

--- run_dev_server.sh
+++ run_dev_server.sh
@@ -3,7 +3,7 @@
 source ./setup.sh
 
 function run_dev_server {
-    gunicorn falcon_web_demo.wsgi:app
+    gunicorn --port=8080 falcon_web_demo.wsgi:app
 }
 
 eval_in_virtual_environment run_dev_server
